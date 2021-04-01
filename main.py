7183849377919139c7bb919097cbd8ebafe34639
Luis <devcorujam@gmail.com>
2021-04-01 14:14:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from collections import deque
+
 def levelOrder(root):
-    queue = [root]
+    queue = deque([root])
     output = []
     
     while queue:
-        first = queue.pop(0)
+        node = queue.popleft()
         
-        output.append(str(first))
+        output.append(str(node))
         
-        if first.left is not None:
-            queue.append(first.left)
-        if first.right is not None:
-            queue.append(first.right)
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
 
     print(" ".join(output))
